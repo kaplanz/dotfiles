0606aecfa69f97fa03cd8e0304d3078bd852ce04
Zakhary Kaplan <zakharykaplan@gmail.com>
2020-07-23 09:10:00 -0400

    Use symlink for lock, remove empty directories

--- utils/.cron/job.sh
+++ utils/.cron/job.sh
@@ -13,6 +13,7 @@ if [ ! "${BASH_VERSINFO[0]}" -ge 4 ]; then
 fi
 
 # Commands
+LN='ln -s'
 MKDIR='mkdir -p'
 
 # Directories
@@ -25,16 +26,18 @@ SCRIPT="$CRON/scripts/$1"
 
 # Run script
 main() {
-    # Ensure job script exists and is executable
+    # Ensure job script exists and is an executable file
     if [ ! -f "$SCRIPT" ] || [ ! -x "$SCRIPT" ]; then
         exit 127 # invalid job
     fi
 
     # Check if job is locked
-    if [ -f "$LOCK" ]; then
+    if [ -f "$LOCK" ] || [ -L "$LOCK" ]; then
         exit 1 # cannot acquire lock
     else
-        touch "$LOCK"
+        # Lock job
+        $MKDIR "$(dirname $LOCK)"
+        $LN "$LOG" "$LOCK" # lock points to log
     fi
 
     # Create directory for log
@@ -46,8 +49,9 @@ main() {
     # Save exit status from job script
     EXIT="${PIPESTATUS[0]}"
 
-    # Remove empty log files
+    # Remove log file (and directory) if empty
     [ ! -s "$LOG" ] && rm "$LOG"
+    rmdir "$(dirname $LOG)" &> /dev/null
 
     # Release lock for job
     rm "$LOCK"
