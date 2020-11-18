60248c772875e003f5f6a7ff8a9f99cfd86cf3c1
Zakhary Kaplan <zakharykaplan@gmail.com>
2020-11-18 18:38:56 -0500

    Increase install.sh verbosity

--- tools/install.sh
+++ tools/install.sh
@@ -7,6 +7,9 @@
 #  Copyright © 2019 Zakhary Kaplan. All rights reserved.
 #
 
+# Installation settings
+DOTFILES=~/.dotfiles
+
 # Check if minimum dependencies are installed
 check_dependencies() {
     {
@@ -23,36 +26,78 @@ check_dependencies() {
 
 # Clone dotfiles repository
 clone_dotfiles_repo() {
-    [ ! -d ~/.dotfiles ] && {
-        git clone https://github.com/zakharykaplan/dotfiles.git ~/.dotfiles
+    [ ! -d "$DOTFILES" ] && {
+        git clone https://github.com/zakharykaplan/dotfiles.git "$DOTFILES"
     }
+}
+
+# Helpers
+print_dependency() {
+    printf "%s $1\n" "$(test $(command -v $1) && echo '✓' || echo '✗')"
+}
+
+print_dependencies() {
+    echo 'Required:'
+    printf '  ' && print_dependency git
+    printf '  ' && print_dependency make
+    printf '  ' && print_dependency stow
+    printf '  ' && print_dependency zsh
+    echo 'Recommended:'
+    printf '  ' && print_dependency tmux
+    printf '  ' && print_dependency vim
 }
 
 # Run script
 main() {
+    # Begin installation
+    echo 'Dofiles Installer'
+    echo
+
     # Check for dependencies
+    echo "Checking dependencies..."
     check_dependencies || {
         # If dependencies not found, do not install
         echo 'Installation cancelled. Please install missing dependencies and try again.'
         [ "$(uname)" = 'Darwin' ] && \
             echo 'Note: Homebrew could be used to satisfy all dependencies.'
         echo
-        echo 'Required:'
-        printf '  %s git\n' "$(test $(command -v git) && echo '✓' || echo '✗')"
-        printf '  %s make\n' "$(test $(command -v make) && echo '✓' || echo '✗')"
-        printf '  %s stow\n' "$(test $(command -v stow) && echo '✓' || echo '✗')"
-        printf '  %s zsh\n' "$(test $(command -v zsh) && echo '✓' || echo '✗')"
-        echo 'Recommended:'
-        printf '  %s tmux\n' "$(test $(command -v tmux) && echo '✓' || echo '✗')"
-        printf '  %s vim\n' "$(test $(command -v vim) && echo '✓' || echo '✗')"
-        return
+        print_dependencies
+        return 1
+    }
+    print_dependencies
+    echo 'Dependencies satisfied. May proceed.'
+    echo
+
+    # Check if already installed
+    echo 'Checking installation path...'
+    echo "DOTFILES = $DOTFILES"
+    [ -f "$DOTFILES" ] || {
+        echo "Installation cancelled. Path not empty."
+        return 1
+    }
+    echo 'Path is empty. May proceed.'
+    echo
+
+    # Confirm installation
+    echo "This will install dotfiles at: $DOTFILES"
+    printf "Proceed with installation [Y/n]? "
+    read PROCEED
+    [ "$PROCEED" = 'N' ] || [ "$PROCEED" = 'n' ] && {
+        return 1
     }
 
     # Clone dotfiles repository
+    echo 'Cloning dotfiles repo...'
     clone_dotfiles_repo
+    echo
 
     # Run Makefile
+    echo 'Running Makefile...'
     make --directory=~/.dotfiles install
+    echo
+
+    # Complete installation
+    echo 'Installation complete. Please use responsibly!'
 }
 
 main "$@"
