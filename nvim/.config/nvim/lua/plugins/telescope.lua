1de6f302bf0eba75d3a24bdce721b1e5446a1a67
Zakhary Kaplan <zakharykaplan@gmail.com>
2022-02-09 20:33:34 -0500

    Reconfigure telescope.nvim

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -8,6 +8,36 @@ require('telescope').setup {
   defaults = {
     -- Default configuration for telescope goes here:
     -- config_key = value,
+    -- Determines the direction "better" results are sorted towards.
+    sorting_strategy = 'ascending',
+    -- Determines the default layout of Telescope pickers.
+    layout_strategy = 'flex',
+    -- Determines the default configuration values for layout strategies.
+    layout_config = {
+      -- Horizontal layout has two columns, one for the preview and one for the
+      -- prompt and results.
+      horizontal = {
+        prompt_position = 'top',
+      },
+      -- Vertical layout stacks the items on top of each other.
+      vertical = {
+        mirror = true,
+        prompt_position = 'top',
+      },
+    },
+    -- Configure winblend for telescope floating windows.
+    winblend = 10,
+    -- The character(s) that will be shown in front of Telescope's prompt.
+    prompt_prefix = ' ',
+    -- The character(s) that will be shown in front of the current selection.
+    selection_caret = '» ',
+    -- Prefix in front of each result entry. Current selection not included.
+    entry_prefix = '  ',
+    -- Symbol to add in front of a multi-selected result entry.
+    multi_icon = '+',
+    -- Boolean defining if borders are added to Telescope windows.
+    border = true,
+    -- Your mappings to override telescope's default mappings.
     mappings = {
       i = {
         -- map actions.which_key to <C-h> (default: <C-/>)
@@ -45,18 +75,18 @@ do
   local opts = { noremap = true, silent = true }
 
   -- Prefix mappings
-  local prefix = '<C-h>'
-  map('n', prefix .. '<CR>', '<Cmd>Telescope<CR>', opts)
-  map('n', prefix .. 'a', '<Cmd>lua require("telescope.builtin").find_files({ hidden = true })<CR>', opts)
-  map('n', prefix .. 'b', '<Cmd>lua require("telescope.builtin").buffers()<CR>', opts)
-  map('n', prefix .. 'f', '<Cmd>lua require("telescope.builtin").find_files()<CR>', opts)
-  map('n', prefix .. 'g', '<Cmd>lua require("telescope.builtin").live_grep()<CR>', opts)
-  map('n', prefix .. 'h', '<Cmd>lua require("telescope.builtin").help_tags()<CR>', opts)
-  map('n', prefix .. 'o', '<Cmd>lua require("telescope.builtin").oldfiles()<CR>', opts)
-  map('n', prefix .. 's', '<Cmd>lua require("telescope.builtin").git_status()<CR>', opts)
+  local prefix = '<C-j>'
+  map('n', prefix, '<Cmd>Telescope<CR>', opts)
+  map('n', prefix .. 'a',    '<Cmd>lua require("telescope.builtin").find_files({ hidden = true })<CR>', opts)
+  map('n', prefix .. 'b',    '<Cmd>lua require("telescope.builtin").buffers()<CR>', opts)
+  map('n', prefix .. 'f',    '<Cmd>lua require("telescope.builtin").find_files()<CR>', opts)
+  map('n', prefix .. 'g',    '<Cmd>lua require("telescope.builtin").live_grep()<CR>', opts)
+  map('n', prefix .. 'h',    '<Cmd>lua require("telescope.builtin").help_tags()<CR>', opts)
+  map('n', prefix .. 'o',    '<Cmd>lua require("telescope.builtin").oldfiles()<CR>', opts)
+  map('n', prefix .. 's',    '<Cmd>lua require("telescope.builtin").git_status()<CR>', opts)
+  map('n', prefix .. prefix, '<Cmd>lua require("telescope.builtin").resume()<CR>', opts)
 
   -- Shortcuts
-  map('n', prefix, prefix .. '<CR>', { silent = true }) -- Telescope
   map('n', '<C-p>', prefix .. 'f', { silent = true })   -- find_files
   map('n', 'gb', prefix .. 'b', { silent = true })      -- buffers
 end
