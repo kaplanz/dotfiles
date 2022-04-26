1bd23d4a785fcc4d87ae86c80c92b233f87a8a91
Zakhary Kaplan <zakharykaplan@gmail.com>
2022-04-26 19:20:24 -0400

    Install shade.nvim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -218,6 +218,12 @@ return require('packer').startup {function()
     end,
   }
   use 'stevearc/dressing.nvim'         -- improve the default vim.ui interfaces
+  use {
+    'sunjon/shade.nvim',               -- dim inactive windows
+    config = function()
+      require('shade').setup()
+    end,
+  }
   -- }}}
 end}
 
