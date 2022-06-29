db519f9e28b24b95bea0e01030f316fa74d275a4
Zakhary Kaplan <zakharykaplan@gmail.com>
2022-06-29 00:00:00 -0400

    feat(nvim): update lspconfig diagnostic symbols

--- apps/nvim/lua/plugins/lspconfig.lua
+++ apps/nvim/lua/plugins/lspconfig.lua
@@ -48,7 +48,7 @@ do
   }
 
   -- Change diagnostic symbols in the sign column (gutter)
-  local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+  local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
   for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
