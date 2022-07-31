11355c663a7f49b4400224a5e15c737fa3eb07c3
Zakhary Kaplan <zakharykaplan@gmail.com>
2022-07-30 23:42:41 -0400

    revert(nvim): use defaults for retrail
    This reverts commit 12c842cc5572fd5cf128549b229ea37bd8beb8e6.

--- apps/nvim/lua/plugins/init.lua
+++ apps/nvim/lua/plugins/init.lua
@@ -255,19 +255,7 @@ return require("packer").startup(function(use)
     use {
       "zakharykaplan/nvim-retrail",
       config = function()
-        require("retrail").setup {
-          filetype = {
-            exclude = {
-              "",
-              "checkhealth",
-              "diff",
-              "help",
-              "man",
-              "mason.nvim",
-              "WhichKey",
-            },
-          },
-        }
+        require("retrail").setup {}
       end,
     }
   end
