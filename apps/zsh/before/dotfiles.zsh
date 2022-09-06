c25626218309d5e9bc673eac2b032bc7a23b2dae
Zakhary Kaplan <me@zakhary.dev>
2022-09-06 15:02:40 -0400

    refactor(zsh): re-order

--- apps/zsh/before/dotfiles.zsh
+++
@@ -1,7 +0,0 @@
-# File:        dotfiles.zsh
-# Author:      Zakhary Kaplan <https://zakhary.dev>
-# Created:     16 May 2021
-# SPDX-License-Identifier: MIT
-
-# Set the path to the dotfiles repo.
-export DOTFILES="$HOME/.dotfiles"
