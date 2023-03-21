7a90ebaa9a731ba95cabec33e1876314dac2d2ff
Zakhary Kaplan <me@zakhary.dev>
2023-03-21 18:30:00 -0400

    feat(wezterm): use config builder

--- apps/wezterm/wezterm.lua
+++ apps/wezterm/wezterm.lua
@@ -6,116 +6,119 @@
 
 local wezterm = require("wezterm")
 
-return {
-  -- Behaviour: {{{
-  exit_behavior = "Close",
-  native_macos_fullscreen_mode = true,
-  -- }}}
+local config = wezterm.config_builder()
 
-  -- Colour: {{{
-  color_scheme = "iceberg-dark",
-  -- }}}
 
-  -- Keymaps {{{
-  keys = {
-    {
-      key = "LeftArrow",
-      mods = "SUPER",
-      action = wezterm.action.SplitPane {
-        direction = "Left",
-      }
-    },
-    {
-      key = "RightArrow",
-      mods = "SUPER",
-      action = wezterm.action.SplitPane {
-        direction = "Right",
-      }
-    },
-    {
-      key = "UpArrow",
-      mods = "SUPER",
-      action = wezterm.action.SplitPane {
-        direction =  "Up",
-      }
-    },
-    {
-      key = "DownArrow",
-      mods = "SUPER",
-      action = wezterm.action.SplitPane {
-        direction =  "Down",
-      }
-    },
-  },
-  -- }}}
+-- Behaviour: {{{
+config.exit_behavior = "Close"
+config.native_macos_fullscreen_mode = true
+-- }}}
 
-  -- Text: {{{
-  allow_square_glyphs_to_overflow_width = "Never",
-  font = wezterm.font "Fira Code",
-  font_size = 13.2,
-  font_rules = {
-    {
-      intensity = "Bold",
-      italic = true,
-      font = wezterm.font {
-        family = "Victor Mono",
-        weight = "Bold",
-        style = "Italic",
-      },
-    },
-    {
-      italic = true,
-      intensity = "Half",
-      font = wezterm.font {
-        family = "Victor Mono",
-        weight = "DemiBold",
-        style = "Italic",
-      },
-    },
-    {
-      italic = true,
-      intensity = "Normal",
-      font = wezterm.font {
-        family = "Victor Mono",
-        style = "Italic",
-      },
-    },
-  },
-  harfbuzz_features = {
-    -- character variants
-    "cv01", -- a
-    "cv02", -- g
-    "cv14", -- 3
-    "cv18", -- %
-    "cv25", -- .-
-    "cv26", -- :-
-    "cv32", -- .=
-    "zero", -- 0
-    -- stylistic sets
-    "ss01", -- r
-    "ss02", -- <= >=
-    "ss04", -- $
-    "ss06", -- \\
-    "ss10", -- Fl Tl fi fj fl ft
-  },
-  text_background_opacity = 0.85,
-  use_cap_height_to_scale_fallback_fonts = true,
-  -- }}}
+-- Colour: {{{
+config.color_scheme = "iceberg-dark"
+-- }}}
 
-  -- Window: {{{
-  hide_tab_bar_if_only_one_tab = true,
-  initial_cols = 96,
-  initial_rows = 32,
-  use_fancy_tab_bar = false,
-  use_resize_increments = true,
-  window_background_opacity = 0.95,
-  window_close_confirmation = "NeverPrompt",
-  window_decorations = "RESIZE",
-  window_padding = {
-    left = 6,
-    right = 6,
-    top = 6,
-    bottom = 6,
+-- Keymaps {{{
+config.keys = {
+  {
+    key = "LeftArrow",
+    mods = "SUPER",
+    action = wezterm.action.SplitPane {
+      direction = "Left",
+    }
   },
-  -- }}}
+  {
+    key = "RightArrow",
+    mods = "SUPER",
+    action = wezterm.action.SplitPane {
+      direction = "Right",
+    }
+  },
+  {
+    key = "UpArrow",
+    mods = "SUPER",
+    action = wezterm.action.SplitPane {
+      direction =  "Up",
+    }
+  },
+  {
+    key = "DownArrow",
+    mods = "SUPER",
+    action = wezterm.action.SplitPane {
+      direction =  "Down",
+    }
+  },
 }
+-- }}}
+
+-- Text: {{{
+config.allow_square_glyphs_to_overflow_width = "Never"
+config.font = wezterm.font "Fira Code"
+config.font_size = 13.2
+config.font_rules = {
+  {
+    intensity = "Bold",
+    italic = true,
+    font = wezterm.font {
+      family = "Victor Mono",
+      weight = "Bold",
+      style = "Italic",
+    },
+  },
+  {
+    italic = true,
+    intensity = "Half",
+    font = wezterm.font {
+      family = "Victor Mono",
+      weight = "DemiBold",
+      style = "Italic",
+    },
+  },
+  {
+    italic = true,
+    intensity = "Normal",
+    font = wezterm.font {
+      family = "Victor Mono",
+      style = "Italic",
+    },
+  },
+}
+config.harfbuzz_features = {
+  -- character variants
+  "cv01", -- a
+  "cv02", -- g
+  "cv14", -- 3
+  "cv18", -- %
+  "cv25", -- .-
+  "cv26", -- :-
+  "cv32", -- .=
+  "zero", -- 0
+  -- stylistic sets
+  "ss01", -- r
+  "ss02", -- <= >=
+  "ss04", -- $
+  "ss06", -- \\
+  "ss10", -- Fl Tl fi fj fl ft
+}
+config.text_background_opacity = 0.85
+config.use_cap_height_to_scale_fallback_fonts = true
+-- }}}
+
+-- Window: {{{
+config.hide_tab_bar_if_only_one_tab = true
+config.initial_cols = 96
+config.initial_rows = 32
+config.use_fancy_tab_bar = false
+config.use_resize_increments = true
+config.window_background_opacity = 0.95
+config.window_close_confirmation = "NeverPrompt"
+config.window_decorations = "RESIZE"
+config.window_padding = {
+  left = 6,
+  right = 6,
+  top = 6,
+  bottom = 6,
+}
+-- }}}
+
+return config
