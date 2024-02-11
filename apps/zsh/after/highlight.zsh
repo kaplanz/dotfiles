51c333e5ccd0761a0fc7873da5ec2c6899d8bde0
Zakhary Kaplan <me@zakhary.dev>
2024-02-11 00:00:00 -0500

    feat(zsh): change prompt {playtime => century}

--- apps/zsh/after/highlight.zsh
+++ apps/zsh/after/highlight.zsh
@@ -3,54 +3,48 @@
 # Created:     19 May 2021
 # SPDX-License-Identifier: MIT
 
-# Define syntax highlighting colurs.
-red="$ZSH_PROMPT_COLOUR_RED"
-green="$ZSH_PROMPT_COLOUR_GREEN"
-yellow="$ZSH_PROMPT_COLOUR_YELLOW"
-blue="$ZSH_PROMPT_COLOUR_BLUE"
-magenta="$ZSH_PROMPT_COLOUR_MAGENTA"
-cyan="$ZSH_PROMPT_COLOUR_CYAN"
+# Create a hash table for globally stashing variables without polluting main
+# scope with a bunch of identifiers.
+typeset -gA HIGHLIGHT
+HIGHLIGHT[RED]=167
+HIGHLIGHT[GRN]=107
+HIGHLIGHT[BLU]=025
+HIGHLIGHT[CYN]=110
+HIGHLIGHT[MGA]=183
+HIGHLIGHT[YLW]=222
+HIGHLIGHT[GRY]=246
 
-# Define syntax highlighting styles.
 ZSH_HIGHLIGHT_STYLES[default]="none"
-ZSH_HIGHLIGHT_STYLES[unknown-token]="fg=$red,bold"
-ZSH_HIGHLIGHT_STYLES[reserved-word]="fg=$yellow"
-ZSH_HIGHLIGHT_STYLES[suffix-alias]="fg=$green,underline"
-ZSH_HIGHLIGHT_STYLES[global-alias]="fg=$cyan"
-ZSH_HIGHLIGHT_STYLES[precommand]="fg=$green,underline"
+ZSH_HIGHLIGHT_STYLES[unknown-token]="fg=$HIGHLIGHT[RED],bold"
+ZSH_HIGHLIGHT_STYLES[reserved-word]="fg=$HIGHLIGHT[YLW]"
+ZSH_HIGHLIGHT_STYLES[suffix-alias]="fg=$HIGHLIGHT[GRN],underline"
+ZSH_HIGHLIGHT_STYLES[global-alias]="fg=$HIGHLIGHT[CYN]"
+ZSH_HIGHLIGHT_STYLES[precommand]="fg=$HIGHLIGHT[GRN],underline"
 ZSH_HIGHLIGHT_STYLES[commandseparator]="none"
-ZSH_HIGHLIGHT_STYLES[autodirectory]="fg=$green,underline"
+ZSH_HIGHLIGHT_STYLES[autodirectory]="fg=$HIGHLIGHT[GRN],underline"
 ZSH_HIGHLIGHT_STYLES[path]=underline
 ZSH_HIGHLIGHT_STYLES[path_pathseparator]=
 ZSH_HIGHLIGHT_STYLES[path_prefix_pathseparator]=
-ZSH_HIGHLIGHT_STYLES[globbing]="fg=$blue"
-ZSH_HIGHLIGHT_STYLES[history-expansion]="fg=$blue"
+ZSH_HIGHLIGHT_STYLES[globbing]="fg=$HIGHLIGHT[BLU]"
+ZSH_HIGHLIGHT_STYLES[history-expansion]="fg=$HIGHLIGHT[BLU]"
 ZSH_HIGHLIGHT_STYLES[command-substitution]="none"
-ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter]="fg=$magenta"
+ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter]="fg=$HIGHLIGHT[MGA]"
 ZSH_HIGHLIGHT_STYLES[process-substitution]="none"
-ZSH_HIGHLIGHT_STYLES[process-substitution-delimiter]="fg=$magenta"
-ZSH_HIGHLIGHT_STYLES[single-hyphen-option]="none"
-ZSH_HIGHLIGHT_STYLES[double-hyphen-option]="none"
+ZSH_HIGHLIGHT_STYLES[process-substitution-delimiter]="fg=$HIGHLIGHT[MGA]"
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]="fd=$HIGHLIGHT[GRY]"
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]="fd=$HIGHLIGHT[GRY]"
 ZSH_HIGHLIGHT_STYLES[back-quoted-argument]="none"
-ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]="fg=$magenta"
-ZSH_HIGHLIGHT_STYLES[single-quoted-argument]="fg=$yellow"
-ZSH_HIGHLIGHT_STYLES[double-quoted-argument]="fg=$yellow"
-ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]="fg=$yellow"
-ZSH_HIGHLIGHT_STYLES[rc-quote]="fg=$cyan"
-ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]="fg=$cyan"
-ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]="fg=$cyan"
-ZSH_HIGHLIGHT_STYLES[back-dollar-quoted-argument]="fg=$cyan"
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]="fg=$HIGHLIGHT[MGA]"
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]="fg=$HIGHLIGHT[GRY]"
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]="fg=$HIGHLIGHT[GRY]"
+ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]="fg=$HIGHLIGHT[YLW]"
+ZSH_HIGHLIGHT_STYLES[rc-quote]="fg=$HIGHLIGHT[CYN]"
+ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]="fg=$HIGHLIGHT[CYN]"
+ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]="fg=$HIGHLIGHT[CYN]"
+ZSH_HIGHLIGHT_STYLES[back-dollar-quoted-argument]="fg=$HIGHLIGHT[CYN]"
 ZSH_HIGHLIGHT_STYLES[assign]="none"
-ZSH_HIGHLIGHT_STYLES[redirection]="fg=$yellow"
-ZSH_HIGHLIGHT_STYLES[comment]="fg=black,bold"
+ZSH_HIGHLIGHT_STYLES[redirection]="fg=$HIGHLIGHT[YLW]"
+ZSH_HIGHLIGHT_STYLES[comment]="fg=$HIGHLIGHT[GRY],bold"
 ZSH_HIGHLIGHT_STYLES[named-fd]="none"
 ZSH_HIGHLIGHT_STYLES[numeric-fd]="none"
-ZSH_HIGHLIGHT_STYLES[arg0]="fg=$green"
-
-# Unset syntax highlighting colurs.
-unset red
-unset green
-unset yellow
-unset blue
-unset magenta
-unset cyan
+ZSH_HIGHLIGHT_STYLES[arg0]="fg=$HIGHLIGHT[GRN]"
