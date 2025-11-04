c09d65eb76fec5428c2f62434195012f4a3223dc
Zakhary Kaplan <me@zakhary.dev>
2025-11-03 20:45:00 -0500

    feat(zoxide): init only if installed

--- apps/zsh/plugin/utils/zoxide.zsh
+++ apps/zsh/plugin/utils/zoxide.zsh
@@ -3,4 +3,10 @@
 # Created:     20 Jun 2024
 # SPDX-License-Identifier: MIT
 
+# Ensure zoxide exists
+if ! command -v zoxide &> /dev/null; then
+    return
+fi
+
+# Initialize zoxide, overriding `cd`
 eval "$(zoxide init --cmd=cd zsh)"
