5678bb6c7b62e935bc274c68b83d8886b901da36
Zakhary Kaplan <me@zakhary.dev>
2025-11-26 23:55:00 -0500

    feat(zsh): add alias: cat=bat

--- apps/zsh/plugin/utils/bat.zsh
+++ apps/zsh/plugin/utils/bat.zsh
@@ -3,4 +3,13 @@
 # Created:     16 May 2021
 # SPDX-License-Identifier: MIT
 
+# Ensure bat exists
+if ! command -v bat &> /dev/null; then
+    return
+fi
+
+# Configure theme
 export BAT_THEME='Nord'
+
+# Hijack cat
+alias cat='bat'
